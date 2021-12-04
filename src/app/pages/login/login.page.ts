54398ff187185b8557b4ee4ef134237754ff93ae
ayman hamdan <hamdan.ayman@hotmail.com>
2021-12-04 19:28:53 +0200

    first message

---
+++ src/app/pages/login/login.page.ts
@@ -0,0 +1,44 @@
+import { Component, OnInit } from '@angular/core';
+import { AlertController } from '@ionic/angular';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.page.html',
+  styleUrls: ['./login.page.scss'],
+})
+export class LoginPage implements OnInit {
+
+  constructor(private alertController :AlertController) { }
+
+  ngOnInit() {
+  }
+  async login(){
+    const alert = await this.alertController.create({
+      cssClass: 'my-custom-class',
+      header: 'are you sure',
+      subHeader: 'login',
+      message: 'This is an alert message.',
+      buttons: [  {
+        text: 'Cancel',
+        role: 'cancel',
+        cssClass: 'secondary',
+        handler: (blah) => {
+          console.log('Confirm Cancel: blah');
+        }
+      }, {
+        text: 'Okay',
+        handler: () => {
+          console.log('Confirm Okay');
+        }
+      }]
+    });
+
+    await alert.present();
+
+    const { role } = await alert.onDidDismiss();
+    console.log('onDidDismiss resolved with role', role);
+  }
+  logout(){
+
+  }
+}
